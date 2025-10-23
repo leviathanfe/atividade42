057d8a93696a684276bcce49c64c0b65a742774a
leviathanfe <levi.fernandes2209@gmail.com>
2025-10-23 13:55:55 -0300

    lógica de programação

---
+++ atividade42.py
@@ -0,0 +1,18 @@
+# Cadastro de Produtos
+# Peça o nome e preço de 5 produtos usando um laço for.
+# Ao final, exiba o nome de todos e a soma total dos preços.
+
+produtos = []
+valores = 0.0
+for i in range(0, 5):
+    produto = input("Digite o nome do produto: ")
+    valor = float(input("Digite o valor: $ "))
+    produtos.append(produto)
+    valores += valor
+
+for nome in produtos:
+    import time
+    print(f"Produto 1: {nome}")
+    time.sleep(0.7)
+    
+print(f"A soma total dos valores é {valores}")
